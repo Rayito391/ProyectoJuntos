4afc22167bad6b860338104e15653e1ece6f9cc6
Rayo <132789085+Rayito391@users.noreply.github.com>
2024-06-17 19:34:52 -0700

    MenuTerminado

--- Frontend/shop/vue-project/src/App.vue
+++ Frontend/shop/vue-project/src/App.vue
@@ -1,16 +1,52 @@
+<template>
+    <div id="app">
+      <div class="background">
+        <div class="overlay"></div>
+      <header>
+        <MenuBarView />
+      </header>
+      <main>
+        <TheWelcome/>
+        <router-view />
+      </main>
+    </div>
+  </div> 
+</template>
+
 <script setup>
-  import {RouterView} from 'vue-router'
-  import MenuBarView from "./views/MenuBarView.vue";
-  import TheWelcome from "./views/TheWelcome.vue";
+import MenuBarView from './views/MenuBarView.vue';
+import TheWelcome from './views/TheWelcome.vue';
 </script>
 
-<template>
-  <header>
-    <MenuBarView/>
-    <TheWelcome/>
-  </header>
-  <RouterView/>
-</template>
-<style scoped>
+<style>
+#app {
+  background: black;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.background{
+  height: 75vh;
+  position: relative;
+  margin-top: 0;
+  background-image: url('@/assets/images/netflixBackground.png');
+  background-position: center;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8); /* Ajusta la opacidad según sea necesario */
+  z-index: 1;
+}
 
+header, main {
+  position: relative;
+  z-index: 2;
+}
+.background{
+  background-color: black;
+}
 </style>
